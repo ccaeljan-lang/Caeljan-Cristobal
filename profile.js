b410338ddee279a12b2fd029508aaaa24ce09e8d
caeljandcristobal <ccaeljan@gmail.com>
2026-09-19 18:27:32 +0800

    Update description

--- profile.js
+++ profile.js
@@ -18,7 +18,7 @@ const profile = {
     // Profile basics
     name: "Caeljan Cristobal",
     username: "You can call me Cj!",
-    bio: "🚀 Into space, astronomy, and computational astrophysics\n🏹💚 Computer Engineering student at DLSU",
+    bio: "🚀 Into space, astronomy, remote sensing, and computational astrophysics\n🏹💚 Computer Engineering student at DLSU",
     profileImage: "images/profile.png",
 
     // Social links - replace URLs with your own
